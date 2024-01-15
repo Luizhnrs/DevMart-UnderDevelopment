04f7a3e085533471ba1ea8aff127dd5b03e95412
Luizhnrs <Luizhnrsg@gmail.com>
2024-01-15 20:01:53 -0300

    TODO: Mobile Sidebar

--- devmart/app/(dashboards)/_components/navbar.tsx
+++ devmart/app/(dashboards)/_components/navbar.tsx
@@ -1,8 +1,8 @@
 export const Navbar  = () => {
   return ( 
     <div className="p-4 border-b h-full flex items-center bg-white shadow-sm ">
-
+      <MobileSidebar/>
     </div>
-   );
-}
+   )
+};
  
