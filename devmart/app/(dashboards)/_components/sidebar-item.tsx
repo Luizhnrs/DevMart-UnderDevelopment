9c10dee34abbcbc45fb77b1a430547b62711dba6
Luizhnrs <Luizhnrsg@gmail.com>
2024-01-15 14:37:01 -0300

    Feat: PathName in SideBar item

---
+++ devmart/app/(dashboards)/_components/sidebar-item.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { LucideIcon } from "lucide-react";
+import { usePathname } from "next/navigation";
+
+interface SidebarItemProps{
+  icon:LucideIcon;
+  label:string;
+  href: string;
+}; 
+
+export const SidebarItem = ({
+  icon: Icon,
+  label,
+  href,
+}: SidebarItemProps) => {
+  const pathname = usePathname();
+
+  return ( 
+    <div>
+      sidebar
+    </div>
+   );
+}
+ 
+export default SidebarItem;
