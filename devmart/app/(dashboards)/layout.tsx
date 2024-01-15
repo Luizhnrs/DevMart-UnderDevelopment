4db95aac6975787950a1965865dd6374877a2054
Luizhnrs <Luizhnrsg@gmail.com>
2024-01-15 15:40:46 -0300

    Feat: Layout Dashboard Update

--- devmart/app/(dashboards)/layout.tsx
+++ devmart/app/(dashboards)/layout.tsx
@@ -8,9 +8,14 @@ const DashboardLayout = ({
   return ( 
     <div className="h-full">
       <div className="hidden md:flex h-full w-56 flex-col fixed inset-y-0 z-0">
+        <div>
+          
+        </div>
         <Sidebar/>
       </div>
+      <main className="md:pl-56 h-full">
       {children}
+      </main>
     </div>
    );
 }
