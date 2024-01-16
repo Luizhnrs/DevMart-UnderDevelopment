6bea5de8ca49e7b711a1805b9d9e176826a82cee
Luizhnrs <Luizhnrsg@gmail.com>
2024-01-15 21:44:41 -0300

    Feat: navbar route for Teacher and customer is implemented

---
+++ devmart/components/navbar-routes.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { UserButton, useAuth } from "@clerk/nextjs";
+import { usePathname, useRouter } from "next/navigation";
+import { LogOut } from "lucide-react";
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
+
+export const NavbarRoutes = () => {
+  const pathname = usePathname();
+  const router = useRouter();
+
+  const isTeacherPage = pathname?.startsWith("/teacher");
+  const isPlayerPage = pathname?.includes("/chapter");
+
+  return (
+    <div className="flex gap-x-2 ml-auto">
+      {isTeacherPage || isPlayerPage ?(
+        <Button size="sm" variant="ghost">
+          <LogOut className="h-4 w-4 mr-2"/>
+          Exit
+        </Button>
+      ):(
+        <Link href="/teacher/courses">
+          <Button size="sm" variant="ghost">
+          Teacher Mode
+          </Button>
+        </Link>
+      )}
+      <UserButton afterSignOutUrl="/"
+      />
+    </div>
+  )
+}
+
