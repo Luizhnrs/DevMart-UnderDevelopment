cb5e4cabcae198f985bd5b540234752f795d8d86
Luizhnrs <Luizhnrsg@gmail.com>
2024-01-15 22:02:14 -0300

    Correction: Navbar path

--- devmart/components/navbar-routes.tsx
+++ devmart/components/navbar-routes.tsx
@@ -16,11 +16,13 @@ export const NavbarRoutes = () => {
 
   return (
     <div className="flex gap-x-2 ml-auto">
-      {isTeacherPage || isPlayerPage ?(
+      {isTeacherPage || isPlayerPage ? (
+         <Link href="/teacher/courses">
         <Button size="sm" variant="ghost">
           <LogOut className="h-4 w-4 mr-2"/>
           Exit
         </Button>
+        </Link>
       ):(
         <Link href="/teacher/courses">
           <Button size="sm" variant="ghost">
