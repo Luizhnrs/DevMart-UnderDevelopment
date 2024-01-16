2ecb4ac4c490fadc6e4e5370da4d554d9ebb7f66
Luizhnrs <Luizhnrsg@gmail.com>
2024-01-16 19:01:04 -0300

    Feat: First API Route implemented

---
+++ devmart/app/api/courses/route.ts
@@ -0,0 +1,18 @@
+import {auth} from "@clerk/nextjs"
+import { NextResponse } from "next/server"
+
+export async function Post(req:Request) {
+  try{
+    const {userId} = auth()
+    const {title} = await req.json()
+
+    if(!userId){
+      return new NextResponse ("Unauthorized", {status:401});
+    }
+
+  }catch(error)
+  {
+    console.log("[COURSES]", error);
+    return new NextResponse ("Internal Error", {status:500})
+  }
+}
