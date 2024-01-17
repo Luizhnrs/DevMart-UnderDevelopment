396a4408716ed66a1e2496ab1f72e25e2048952c
Luizhnrs <Luizhnrsg@gmail.com>
2024-01-17 15:53:37 -0300

    Feat: Completed Fields dashboard in create course id

--- devmart/app/(dashboards)/(routes)/teacher/courses/[courseid]/page.tsx
+++ devmart/app/(dashboards)/(routes)/teacher/courses/[courseid]/page.tsx
@@ -1,6 +1,8 @@
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
+
+
 const CourseIdPage = async ({
   params
 }:{
@@ -9,23 +11,48 @@ const CourseIdPage = async ({
   const {userId} = auth();
 
   if(!userId){
-    return redirect
+    return redirect("/")
   }
 
   const course = await db.course.findUnique({
     where:{
       id: params.courseId
     }
-  })
+  });
 
   if(!course){
     return redirect("/")
   }
 
+  const requiredFields  = [
+    course.title,
+    course.description,
+    course.imageUrl,
+    course.price,
+    course.categoryId
+  ];
+
+  const totalFields = requiredFields.length;
+  const completedFields = requiredFields.filter(Boolean).length;
+
+  const completitionText = `(${completedFields}/${totalFields})`
+
   return ( 
-    <div>
-      Course Id: {params.courseId}
+    <div className="p-6">
+      <div className="flex items-center justify-between">
+        <div className="flex flex-col gap-y-2">
+            <h1 className="text-2xl font-medium">
+              Course Setup
+            </h1>
+            <span className="text-sm text-slate-700">
+              Complete all fields {completitionText}
+            </span>
+        </div>
+       </div>
+      <div className="">
+
     </div>
+  </div>
    );
 }
  
