3fe7c660dffa6172428967ccd8365d6286c0cb36
Luizhnrs <Luizhnrsg@gmail.com>
2024-01-17 14:28:46 -0300

    Feat: UserID validation

--- devmart/app/(dashboards)/(routes)/teacher/courses/[courseid]/page.tsx
+++ devmart/app/(dashboards)/(routes)/teacher/courses/[courseid]/page.tsx
@@ -1,7 +1,26 @@
-const CourseIdPage = () => {
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/dist/server/api-utils";
+const CourseIdPage = async ({
+  params
+}:{
+  params: {courseId: string}
+}) => {
+  const {userId} = auth();
+
+  if(!userId){
+    return redirect
+  }
+
+  const course = await db.course.findUnique({
+    where:{
+      id: params.courseId
+    }
+  })
+
   return ( 
     <div>
-      Course Id Page
+      Course Id: {params.courseId}
     </div>
    );
 }
