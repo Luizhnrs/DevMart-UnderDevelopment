34e68c1c1c8370f52925a65481ca73807e69cf4a
Luizhnrs <Luizhnrsg@gmail.com>
2024-01-17 16:58:43 -0300

    Feat: Icon Variants in icon badge implemented

--- devmart/components/icon-badge.tsx
+++ devmart/components/icon-badge.tsx
@@ -22,3 +22,23 @@ const backgroundVariants = cva(
     }
   }
 );
+
+const iconVariants = cva(
+  "",
+  {
+    variants: {
+      variant: {
+        default: "text-sky-700",
+        success: "text-emerald-700",
+      },
+      size: {
+        default: "h-8 w-8",
+        sm: "h-4 w-4"
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+      size: "default",
+    }
+  }
+);
