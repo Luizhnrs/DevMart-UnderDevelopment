fdf1fe1922f9d261768fa467880e716bf6451881
Luizhnrs <Luizhnrsg@gmail.com>
2024-01-18 18:48:22 -0300

    TODO: Description Form

--- devmart/app/(dashboards)/(routes)/teacher/courses/[courseid]/page.tsx
+++ devmart/app/(dashboards)/(routes)/teacher/courses/[courseid]/page.tsx
@@ -3,6 +3,7 @@ import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
 import { LayoutDashboard } from "lucide-react";
 import { redirect } from "next/navigation";
+import { TitleForm } from "./_components/title-form";
 
 
 const CourseIdPage = async ({
@@ -60,6 +61,10 @@ const CourseIdPage = async ({
           </h2>
         </div>
         <TitleForm
+          initialData={course}
+          courseId = {course.id}
+        />
+        <DescriptionForm
           initialData={course}
           courseId = {course.id}
         />
