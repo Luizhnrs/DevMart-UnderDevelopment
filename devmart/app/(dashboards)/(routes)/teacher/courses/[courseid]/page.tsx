a542af83543e9e1bd63cf82dc5feadf3d23af849
Luizhnrs <Luizhnrsg@gmail.com>
2024-01-24 20:51:10 -0300

    Feat: Icon Badge in course ID page

--- devmart/app/(dashboards)/(routes)/teacher/courses/[courseid]/page.tsx
+++ devmart/app/(dashboards)/(routes)/teacher/courses/[courseid]/page.tsx
@@ -67,6 +67,7 @@ const CourseIdPage = async ({
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
         <div>
           <div className="flex items-center gap-x-2">
+            <IconBadge icon={LayoutDashboard}/>
             <h2 className="text-xl">
               Customize your course
             </h2>
