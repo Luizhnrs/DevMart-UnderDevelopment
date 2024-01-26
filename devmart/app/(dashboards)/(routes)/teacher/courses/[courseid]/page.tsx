8347dd8f56919892c740fd02956677e06afed913
Luizhnrs <Luizhnrsg@gmail.com>
2024-01-26 16:50:47 -0300

    Feat: Category Form implemented in main page(customize your course)

--- devmart/app/(dashboards)/(routes)/teacher/courses/[courseid]/page.tsx
+++ devmart/app/(dashboards)/(routes)/teacher/courses/[courseid]/page.tsx
@@ -13,6 +13,8 @@ import { IconBadge } from "@/components/icon-badge";
 import { TitleForm } from "./_components/title-form";
 import { DescriptionForm } from "./_components/description-form";
 import { ImageForm } from "./_components/image-form";
+import { CategoryForm } from "./_components/category-form";
+
 
 const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
   const { userId } = auth();
@@ -72,6 +74,10 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
           <TitleForm initialData={course} courseId={course.id} />
           <DescriptionForm initialData={course} courseId={course.id} />
           <ImageForm initialData={course} courseId={course.id} />
+          <CategoryForm initialData={course} courseId={course.id} options={categories.map((category) => ({
+            label:category.name,
+            value:category.id,
+          }))}/>
         </div>
       </div>
     </div>
